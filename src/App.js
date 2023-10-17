a2ac5b0e229ed506f4d1568272a3008932110479
Burak Güvenç <burak.guvenc@cimri.com>
2023-10-17 17:15:24 +0300

    -- kafa yandi abijim ben off, gece editlerim.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,33 @@ function App() {
     const [board, setBoard] = useState(boardDefault);
     const [currAttempt, setCurrAttempt] = useState({attempt: 0, letterPos: 0});
 
+
+    /* asagidaki function icindekiler KEY.JS'in icinde 'if' 'else' conditiondaydi
+    * ordan tasiyip niye buraya getirdik?
+    * buraya tasimadan once  asagidaki 3 const'u olusturduk.
+    * */
+
+    const onSelectLetter = (keyVal) => {
+        if (currAttempt.letterPos > 4)  return;
+        const newBoard = [...board];
+        newBoard [currAttempt.attempt][currAttempt.letterPos] = keyVal;
+        setBoard(newBoard);
+        setCurrAttempt({...currAttempt, letterPos: currAttempt.letterPos + 1});
+    }
+
+    const onDelete = () => {
+        if (currAttempt.letterPos === 0) return;
+        const newBoard = [...board];
+        newBoard [currAttempt.attempt][currAttempt.letterPos - 1] = "";
+        setBoard(newBoard);
+        setCurrAttempt({...currAttempt, letterPos: currAttempt.letterPos - 1});
+    }
+
+    const onEnter = () => {
+        if (currAttempt.letterPos !== 5) return;
+        setCurrAttempt({attempt: currAttempt.attempt + 1, letterPos: 0});
+    }
+
     return (
     <div className="App">
       <nav>
